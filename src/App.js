ca47b083868f096cb1a1e6d0eef7196952177d64
Olivia Park <olivia.park1346@gmail.com>
2020-08-05 11:23:28 +0900

    update to app.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route
+} from 'react-render-dom';
+
+const App = () => (
+  <BrowserRouter>
+
+  </BrowserRouter>
+);
+
+export default App;
